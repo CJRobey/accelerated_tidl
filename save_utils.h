cd314e9eed9d97b0c458c927705096c1754cda5a
Caleb Robey <a0232989@ti.com>
2020-02-19 16:55:08 -0600

    helpful debugging tools

---
+++ save_utils.h
@@ -0,0 +1,11 @@
+#include <string>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <fstream>
+
+void printType(cv::Mat &mat);
+void printInfo(cv::Mat &mat);
+void save_data(void *img_data, int w, int h, const int out_channels, const int in_channels);
+void write_binary_file(void *data, std::string name, unsigned int size);
